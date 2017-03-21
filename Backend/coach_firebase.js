614ebaf17aba99bae896da536b6216fa91b01fbb
Kakit Ho <hkk565@gmail.com>
2017-03-22 03:01:26 +0800

    Remove old code

--- Backend/coach_firebase.js
+++
@@ -1,9 +0,0 @@
-// Initialize Firebase
-var config = {
-	apiKey: "AIzaSyDJEEj878f_MgnhJs-0Kx-F09i7UgN-V28",
-	authDomain: "coach-bcb1c.firebaseapp.com",
-	databaseURL: "https://coach-bcb1c.firebaseio.com",
-	storageBucket: "coach-bcb1c.appspot.com",
-	messagingSenderId: "1051823682313"
-};
-firebase.initializeApp(config);
